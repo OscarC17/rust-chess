b50d00abb0df0a3aaaceac72fe120d822a3980e1
BigAaron1243 <bigaaron1243@gmail.com>
2023-05-11 23:59:20 +1000

    rust

---
+++ main.rs
@@ -0,0 +1,47 @@
+use std::io;
+fn main() {
+
+    println!("Chess :)");
+
+    let mut array: [char; 64] = ['☐'; 64];
+
+    let black: [char; 16] = ['R', 'H', 'B', 'Q', 'K', 'B', 'H', 'R', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'];
+    let white: [char; 16] = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'r', 'h', 'b', 'q', 'k', 'b', 'h', 'r'];
+    
+    for i in 0..64 {
+        if i < 16 {
+            array[i] = black[i];
+        } 
+        if i >= 48 {
+            array[i] = white[i - 48];
+        }
+    }
+
+    let stdin = io::stdin();
+    loop {
+        print!("  ");
+        for i in 0..=8 {
+            for j in 0..=7 {
+                if i == 0 {
+                    print!("{} ", (j as u8 + 65) as char);
+                } else {
+                    print!("{} ", array[(i-1) * 8 + j]);
+                }
+            }
+            if i != 8 {
+                print!("\n{} ", (8 - i as u8 + 48) as char);
+            } else {
+                print!("\n");
+            }
+        }
+        println!("Select piece: ");
+        let mut u_in = String::new();
+        match io::stdin().read_line(&mut u_in) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        println!("{:?}, {:?}", u_in.chars().nth(0), u_in.chars().nth(1));
+        println!("{}", u_in);
+        println!("");
+    }
+}
