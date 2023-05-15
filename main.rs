9a7b5a75c24b3a443b80b7ab732b4c0d66491d7f
BigAaron1243 <bigaaron1243@gmail.com>
2023-05-15 15:45:46 +1000

    checkers

--- main.rs
+++ main.rs
@@ -1,8 +1,19 @@
 use std::io;
+use std::collections::HashMap;
+
 fn main() {
 
     println!("Chess :)");
+    //println!("{}, {}, {}, {}, {}", get_x_index(0), get_x_index(1), get_x_index(2), get_x_index(3), get_x_index(4));
+    //println!("{}, {}, {}, {}, {}", get_y_index(0), get_y_index(1), get_y_index(2), get_y_index(3), get_y_index(4));
 
+
+    let top = "        A     B     C     D     E     F     G     H";
+    let vert_line = "     -------------------------------------------------";
+    let w_line = "     ██████      ██████      ██████      ██████       ";
+    let b_line = "           ██████      ██████      ██████      ██████ ";
+
+    //println!("\n{}\n", top);
 
     // Create empty array to store board state
     let mut array: [char; 64] = [' '; 64];
@@ -22,16 +33,60 @@ fn main() {
 
 
 
-    let stdin = io::stdin();
-    let mut iterator: u8 = 0;
-    for piece in array { 
-        if iterator % 8 == 0 {
-            println!("----------------------------------------------");
+    let char_piece_art_assoc = Hashmap::from([
+        ("p", "()"),
+        ("r", "uu"),
+        ("h", "|>"),
+        ("b", "{}"),
+        ("q", "vv"),
+        ("k", "++"),
+        ("P", "()"),
+        ("R", "nn"),
+        ("H", "<|"),
+        ("B", "{}"),
+        ("Q", "^^"),
+        ("K", "++"),
+    ]);
+
+
+    // Part A: create array of type String.
+    // ... Use to_string for elements.
+    let mut board: Vec<String> = vec![String::new(); 27];
+
+    board[0] = "".to_string();
+    board[1] = top.to_string();
+    board[2] = "".to_string();
+    for i in 0..=7 {
+        let mut active_line: String = "no".to_string(); 
+        if i % 2 == 0 {
+            active_line = w_line.to_string();
+        } else {
+            active_line = b_line.to_string();
         }
-        iterator += 1;
+        let mut active_line_with_num = active_line.clone();
+        let mut gooba = format!("{}", 8 - i);
+        active_line_with_num.replace_range(2..3, gooba.as_str());
+        println!("{}, ", i);
+        board[i * 3 + 0 + 3] = active_line.clone();
+        //board[i * 4 + 1 + 3] = active_line.clone();
+        board[i * 3 + 1 + 3] = active_line_with_num.to_string();
+        board[i * 3 + 2 + 3] = active_line.clone();
+    }
+
+    //board[27] = vert_line.to_string();
+    
+    // Part B: print elements.
+    for value in &board {
+        println!("{}", value);
     }
 
     loop {
+
+        // Generate the pieces of the board
+       /* 
+        for i in 
+
+        
         print!("  ");
         for i in 0..=8 {
             for j in 0..=7 {
@@ -47,6 +102,7 @@ fn main() {
                 print!("\n");
             }
         }
+        */
         println!("Select piece: ");
         let mut u_in = String::new();
         match io::stdin().read_line(&mut u_in) {
@@ -57,4 +113,12 @@ fn main() {
         println!("{}", u_in);
         println!("");
     }
+}
+
+fn get_x_index(x: i32) -> i32 {
+    x * 6 + 8
+}
+
+fn get_y_index(y: i32) -> i32 {
+    y * 4 + 5
 }
