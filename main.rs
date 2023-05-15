d23ea3e3a9ab8f4e2014874def0b39edd69bcc37
BigAaron1243 <bigaaron1243@gmail.com>
2023-05-15 18:04:43 +1000

    now you can move the pieces

--- main.rs
+++ main.rs
@@ -3,15 +3,16 @@ use std::collections::HashMap;
 
 fn main() {
 
-    println!("Chess :)");
     //println!("{}, {}, {}, {}, {}", get_x_index(0), get_x_index(1), get_x_index(2), get_x_index(3), get_x_index(4));
     //println!("{}, {}, {}, {}, {}", get_y_index(0), get_y_index(1), get_y_index(2), get_y_index(3), get_y_index(4));
 
 
     let top = "        A     B     C     D     E     F     G     H";
     let vert_line = "     -------------------------------------------------";
-    let w_line = "     ██████      ██████      ██████      ██████       ";
-    let b_line = "           ██████      ██████      ██████      ██████ ";
+    //let w_line = "     ██████      ██████      ██████      ██████       ";
+    //let b_line = "           ██████      ██████      ██████      ██████ ";
+    let w_line = "     ######      ######      ######      ######       ";
+    let b_line = "           ######      ######      ######      ###### ";
 
     //println!("\n{}\n", top);
 
@@ -33,7 +34,7 @@ fn main() {
 
 
 
-    let char_piece_art_assoc = Hashmap::from([
+    let char_piece_art_assoc = HashMap::from([
         ("p", "()"),
         ("r", "uu"),
         ("h", "|>"),
@@ -46,6 +47,7 @@ fn main() {
         ("B", "{}"),
         ("Q", "^^"),
         ("K", "++"),
+        (" ", ""),
     ]);
 
 
@@ -53,34 +55,54 @@ fn main() {
     // ... Use to_string for elements.
     let mut board: Vec<String> = vec![String::new(); 27];
 
-    board[0] = "".to_string();
-    board[1] = top.to_string();
-    board[2] = "".to_string();
-    for i in 0..=7 {
-        let mut active_line: String = "no".to_string(); 
-        if i % 2 == 0 {
-            active_line = w_line.to_string();
-        } else {
-            active_line = b_line.to_string();
-        }
-        let mut active_line_with_num = active_line.clone();
-        let mut gooba = format!("{}", 8 - i);
-        active_line_with_num.replace_range(2..3, gooba.as_str());
-        println!("{}, ", i);
-        board[i * 3 + 0 + 3] = active_line.clone();
-        //board[i * 4 + 1 + 3] = active_line.clone();
-        board[i * 3 + 1 + 3] = active_line_with_num.to_string();
-        board[i * 3 + 2 + 3] = active_line.clone();
-    }
-
-    //board[27] = vert_line.to_string();
-    
-    // Part B: print elements.
-    for value in &board {
-        println!("{}", value);
-    }
-
     loop {
+
+        board[0] = "".to_string();
+        board[1] = top.to_string();
+        board[2] = "".to_string();
+        for i in 0..=7 {
+            let mut active_line: String = "no".to_string(); 
+            if i % 2 == 0 {
+                active_line = w_line.to_string();
+            } else {
+                active_line = b_line.to_string();
+            }
+            let mut active_line_with_num = active_line.clone();
+            let mut active_line_top = active_line.clone();
+            let mut active_line_bottom = active_line.clone();
+            let mut gooba = format!("{}", 8 - i);
+            active_line_with_num.replace_range(2..3, gooba.as_str());
+            for j in 0..=7 {
+               // println!("3");
+               // println!("{}", array[i as usize * 8 + j as usize]);
+               // println!("{}", char_piece_art_assoc[&array[i as usize * 8 + j as usize].to_string() as &str]);
+               // println!("{}, {}", get_x_index(j), get_x_index(j) + 2);
+                if (array[i as usize * 8 + j as usize] != ' ') {
+                    active_line_with_num.replace_range(get_x_index(j)..(get_x_index(j) + 2), char_piece_art_assoc[&array[i as usize * 8 + j as usize].to_string() as &str]);
+                }
+                if array[i as usize * 8 + j as usize] > 96 as char {
+                    active_line_bottom.replace_range(get_x_index(j)..(get_x_index(j) + 2), "][");
+                } else if array[i as usize * 8 + j as usize] != 32 as char {
+                    active_line_top.replace_range(get_x_index(j)..(get_x_index(j) + 2), "][");
+                }
+                //active_line_with_num.replace_range(7..8, &char_piece_art_assoc[&array[i as usize * 8 + j as usize].to_string() as &str]);
+                //active_line_with_num.replace_range(3..5, "yy");
+                
+            }
+            //println!("{}, ", i);
+            board[i as usize * 3 + 0 + 3] = active_line_top.to_string();
+            //board[i * 4 + 1 + 3] = active_line.clone();
+            board[i as usize * 3 + 1 + 3] = active_line_with_num.to_string();
+            board[i as usize * 3 + 2 + 3] = active_line_bottom.to_string();
+        }
+
+        //board[27] = vert_line.to_string();
+        
+        // Part B: print elements.
+        for value in &board {
+            println!("{}", value);
+        }
+
 
         // Generate the pieces of the board
        /* 
@@ -109,16 +131,35 @@ fn main() {
             Ok(_goes_into_input_above) => {},
             Err(_no_updates_is_fine) => {},
         }
-        println!("{:?}, {:?}", u_in.chars().nth(0), u_in.chars().nth(1));
-        println!("{}", u_in);
+        //println!("{:?}, {:?}", u_in.chars().nth(0), u_in.chars().nth(1));
+        println!("{}, {}", letter_to_num(u_in.chars().nth(0).unwrap()), 8 - (u_in.chars().nth(1).unwrap() as usize - 48 as usize));
+        let mut from_index = letter_to_num(u_in.chars().nth(0).unwrap()) + 8 * (8 - (u_in.chars().nth(1).unwrap() as usize - 48 as usize));
+        println!("{}", array[from_index]);
         println!("");
+
+        u_in = String::new();
+        match io::stdin().read_line(&mut u_in) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        let mut to_index = letter_to_num(u_in.chars().nth(0).unwrap()) + 8 * (8 - (u_in.chars().nth(1).unwrap() as usize - 48 as usize));
+        array[to_index] = array[from_index];
+        array[from_index] = ' ';
     }
 }
 
-fn get_x_index(x: i32) -> i32 {
-    x * 6 + 8
+fn get_x_index(x: i32) -> usize {
+    (x * 6 + 7) as usize
 }
 
-fn get_y_index(y: i32) -> i32 {
-    y * 4 + 5
+fn get_y_index(y: i32) -> usize {
+    (y * 4 + 5) as usize
+}
+
+fn letter_to_num(alpha: char) -> usize {
+    if alpha as usize >= 97 {
+        return alpha as usize - 97;
+    } else {
+        return alpha as usize - 65;
+    }
 }
